b54d2ccdc8bc102b543148743601207f0612d47a
Alasdair Smith <alasdairsmith0491@gmail.com>
2020-07-01 09:24:35 +0100

    Refactor tests to parameterised form

--- test/mars-rover.test.ts
+++ test/mars-rover.test.ts
@@ -18,12 +18,6 @@ describe("MarsRover", () => {
         expect(location).toEqual("0,0,N");
     });
 
-    it("should move forwards to location 0,1,N", () => {
-        const marsRover = new MarsRover();
-        const location = marsRover.run("M");
-        expect(location).toEqual("0,1,N");
-    });
-
     it("should move forwards twice to location 0,2,N", () => {
         const marsRover = new MarsRover();
         const location = marsRover.run("MM");
@@ -35,4 +29,22 @@ describe("MarsRover", () => {
         const location = marsRover.run("MMM");
         expect(location).toEqual("0,3,N");
     });
+
+    it("should move over the northern edge to return to the bottom", () => {
+        const marsRover = new MarsRover();
+        const location = marsRover.run("MMMMMMMMMM");
+        expect(location).toEqual("0,0,N");
+    });
+
+    it.each([
+        ["M", "0,1,N"],
+        ["MM", "0,2,N"],
+        ["MMM", "0,3,N"],
+        ["MMMMMMMMMM", "0,0,N"],
+    ])("It moves north, input: %s, expected output: %s", (input, expectedOutput) => {
+        const marsRover = new MarsRover();
+        const location = marsRover.run(input);
+        expect(location).toEqual(expectedOutput);
+    })
+
 });
